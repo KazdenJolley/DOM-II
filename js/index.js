c5825e8df28ccaf43227ee7b7d4b70820e9981e7
kazden.jolley <kazden.jolley@gmail.com>
2021-05-04 15:27:00 -0600

    added mouseover, keydown, and wheel event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+const navHeader = document.querySelector('.main-navigation');
+function blueBG(event) {
+    navHeader.style.backgroundColor = 'blue';
+}
+function whiteBG(event) {
+    if (event.key === 'w'){
+        navHeader.style.backgroundColor = 'white';
+    }
+}
+navHeader.addEventListener('mouseover', blueBG);
+document.addEventListener('keydown', whiteBG);
+
+const busImg = document.querySelector('.intro img');
+let scale = 0;
+function rotate(event) {
+    event.preventDefault();
+    scale += event.deltaY * -1;
+    busImg.style.transform = `rotate(${scale}deg)`
+}
+busImg.addEventListener('wheel', rotate);
