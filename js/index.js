bf87bf906d3d4210d1cfc195d65e8d4821537349
kazden.jolley <kazden.jolley@gmail.com>
2021-05-04 16:27:06 -0600

    added 6th event listener

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ function whiteBG(event) {
         navHeader.style.backgroundColor = 'white';
     }
 }
+    // 1
 navHeader.addEventListener('mouseover', blueBG);
+    // 2
 document.addEventListener('keydown', whiteBG);
-
 const busImg = document.querySelector('.intro img');
 let scale = 0;
 function rotate(event) {
@@ -18,12 +19,28 @@ function rotate(event) {
     scale += event.deltaY * -1;
     busImg.style.transform = `rotate(${scale}deg)`
 }
+    // 3
 busImg.addEventListener('wheel', rotate);
-
 const busTitle = document.querySelector('.intro h2');
 function large(event) {
     debugger;
     event.preventDefault();
     busTitle.style.fontSize = '6rem';
 }
+    // 4
 busTitle.addEventListener('dblclick', large);
+function small(event) {
+    busTitle.style.fontSize = '3rem';
+}
+    // 5
+busTitle.addEventListener('mouseout', small);
+adventureAwaits = document.querySelector('.home section:nth-of-type(2) div:nth-of-type(2)')
+let opacity = 1;
+function blur(event) {
+    if(opacity >= 0){
+        opacity -= 0.01;
+        adventureAwaits.style.opacity = `${opacity}`;
+    }
+}
+    // 6
+adventureAwaits.addEventListener('mousemove', blur);
