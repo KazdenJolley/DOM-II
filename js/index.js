3cd069bd0b2a890979abb07e7d1d3613918d90d6
kazden.jolley <kazden.jolley@gmail.com>
2021-05-04 17:03:36 -0600

    added rest of event listeners

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // Your code goes here
+    // 1
 const navHeader = document.querySelector('.main-navigation');
 function blueBG(event) {
     navHeader.style.backgroundColor = 'blue';
@@ -8,10 +9,12 @@ function whiteBG(event) {
         navHeader.style.backgroundColor = 'white';
     }
 }
-    // 1
 navHeader.addEventListener('mouseover', blueBG);
+
     // 2
 document.addEventListener('keydown', whiteBG);
+
+    // 3
 const busImg = document.querySelector('.intro img');
 let scale = 0;
 function rotate(event) {
@@ -19,22 +22,25 @@ function rotate(event) {
     scale += event.deltaY * -1;
     busImg.style.transform = `rotate(${scale}deg)`
 }
-    // 3
 busImg.addEventListener('wheel', rotate);
+
+    // 4
 const busTitle = document.querySelector('.intro h2');
 function large(event) {
     debugger;
     event.preventDefault();
     busTitle.style.fontSize = '6rem';
 }
-    // 4
 busTitle.addEventListener('dblclick', large);
+
+    // 5
 function small(event) {
     busTitle.style.fontSize = '3rem';
 }
-    // 5
 busTitle.addEventListener('mouseout', small);
-adventureAwaits = document.querySelector('.home section:nth-of-type(2) div:nth-of-type(2)')
+
+    // 6
+const adventureAwaits = document.querySelector('.home section:nth-of-type(2) div:nth-of-type(2)')
 let opacity = 1;
 function blur(event) {
     if(opacity >= 0){
@@ -42,5 +48,30 @@ function blur(event) {
         adventureAwaits.style.opacity = `${opacity}`;
     }
 }
-    // 6
 adventureAwaits.addEventListener('mousemove', blur);
+
+    // 7
+const sunButton = document.querySelector('.btn');
+function red(event) {
+    event.preventDefault();
+    sunButton.style.backgroundColor = 'orange';
+}
+sunButton.addEventListener('focus', red);
+
+    // 8
+window.addEventListener('resize', (event) => {
+    document.querySelector('body').style.backgroundColor = 'lightgrey';
+})
+
+    // 9 
+const mapImg = document.querySelector('.img-content img');
+function remove(event) {
+    mapImg.remove();
+}
+document.addEventListener('scroll', remove);
+
+    // 10
+const title = document.querySelector('title');
+document.addEventListener('DOMContentLoaded', (event) => {
+    title.textContent = 'Look at me!!';
+})
